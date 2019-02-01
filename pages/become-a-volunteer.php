7ea1f19bfd6573ad70a1b171131fe987883ce7b2
Anthony Earles <tony@icgonline.co.uk>
2019-02-01 16:10:46 +0000

    New layout and JS

--- pages/become-a-volunteer.php
+++ pages/become-a-volunteer.php
@@ -1,15 +1,20 @@
-<h3>Become A Volunteer</h3>
-<figure class="row">
-    <picture>
-        <img src="<?= $this->sitepath('images/volunteer-desktop.jpg') ?>">
-    </picture>
-    <figcaption>
-        <p><strong>As a registered charity we rely entirely on a team of dedicated volunteers to ensure we can continue to offer our facilities to the residents of Hesketh Bank, Tarleton, Banks and beyond.</strong></p>
 
-        <p>If you have an hour to spare each week then get in touch. Our volunteer activities range from meeting current and potential users and ensuring the <a href="<?= $this->sitepath('our-facilities/muga') ?>">MUGA</a> is open and available, to ensuring users have the equipment (tables, chairs etc) that they need.</p>
+ <figure class="fullwidth header">
+          <img src="<?= $this->sitepath('images/volunteer-desktop.jpg') ?>">
+        <figcaption>
+            <h1>Become A Volunteer</h1>
+        </figcaption>    
+    </figure>
+
+
+<section class="onecol">
+   
+ <p><strong>As a registered charity we rely entirely on a team of dedicated volunteers to ensure we can continue to offer our facilities to the residents of Hesketh Bank, Tarleton, Banks and beyond.</strong></p>
+       <p>If you have an hour to spare each week then get in touch. Our volunteer activities range from meeting current and potential users and ensuring the <a href="<?= $this->sitepath('our-facilities/muga') ?>">MUGA</a> is open and available, to ensuring users have the equipment (tables, chairs etc) that they need.</p>
 
         <p>Volunteering is a great way to meet people around the village and would suit someone retired, semi-retired or just stuck at home.</p>
 
         <p>If you want to know more then email <a href="info@heskethbankcommunitycentre.org.uk">info@heskethbankcommunitycentre.org.uk</a> or call us on <strong>07784 942 127</strong>.</p>
-    </figcaption>
-</figure>
+        
+ 
+</section>
