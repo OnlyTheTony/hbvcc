2e2c3575eb4885f872238d0463d678598fa96492
Anthony Earles <tony@icgonline.co.uk>
2020-04-15 13:46:48 +0100

    New homepage layout and CSS

--- .history/js/core_20190821165604.js
+++ .history/js/core_20190821165604.js
@@ -15,19 +15,19 @@ window.addEventListener('DOMContentLoaded', function () {
 
     new Core();
     var dt = new Date(Date.now()).toISOString();
-    // formatGoogleCalendar.init({calendarUrl: 'https://www.googleapis.com/calendar/v3/calendars/heskethbankcommunitycentre.org.uk_3i6pu2nj7e67l9snfpk2tss75c@group.calendar.google.com/events?orderBy=startTime&showDeleted=false&singleEvents=true&timeMin=' + dt + '&key=AIzaSyDTdTCaKV8grIqu18s8XQ4n5QvCqDnwb5w',
-    //     past: false,
-    //     upcoming: true,
-    //     sameDayTimes: true,
-    //     dayNames: true,
-    //     pastTopN: -1,
-    //     upcomingTopN: 20,
-    //     recurringEvents: true,
-    //     itemsTagName: 'li',
-    //     upcomingSelector: '#upcoming-events',
-    //     format: ['*date*', '*summary*'],
-    //     timeMin: undefined,
-    //     timeMax: undefined});
+    formatGoogleCalendar.init({calendarUrl: 'https://www.googleapis.com/calendar/v3/calendars/heskethbankcommunitycentre.org.uk_3i6pu2nj7e67l9snfpk2tss75c@group.calendar.google.com/events?orderBy=startTime&showDeleted=false&singleEvents=true&timeMin=' + dt + '&key=AIzaSyDTdTCaKV8grIqu18s8XQ4n5QvCqDnwb5w',
+        past: false,
+        upcoming: true,
+        sameDayTimes: true,
+        dayNames: true,
+        pastTopN: -1,
+        upcomingTopN: 20,
+        recurringEvents: true,
+        itemsTagName: 'li',
+        upcomingSelector: '#upcoming-events',
+        format: ['*date*', '*summary*'],
+        timeMin: undefined,
+        timeMax: undefined});
 
 
 /* handle the hover function for the menu */
---
+++ .history/pages/index_20191119131243.php
@@ -0,0 +1,60 @@
+<article id="bannerholder" class="">
+    <div class="cta" id="banner-run" data-delay="5000">
+<picture>
+    <source media="(min-width:769px)" srcset="<?= $this->sitepath('images/hesketh-bank-community-centre.jpg') ?>">
+    <source media="(max-width:768px)" srcset="<?= $this->sitepath('images/hesketh-bank-community-centre-mid.jpg') ?>">
+    <source media="(max-width:640px)" srcset="<?= $this->sitepath('images/hesketh-bank-community-centre-sm.jpg') ?>">
+    <source media="(max-width:425px)" srcset="<?= $this->sitepath('images/hesketh-bank-community-centre-xs.jpg') ?>">
+    <img src="<?= $this->sitepath('images/hesketh-bank-community-centre.jpg') ?>" class="banner">
+</picture>
+      
+    </div>
+</article>
+<section class="three-col">
+    <section id="fundraiser">
+        <h4>Love Hesketh Bank</h4>
+        <p>We are featured in the November issue of the Hesketh Bank Parish Council newsletter. Download it <a href="<?= $this->sitepath('assets/downloads/Newsletter-Nov-19.pdf') ?>" download="Love Hesketh Bank Newsletter - November 2019.pdf">here</a>.</p>
+        
+         <h4>Latest Newsletter</h4>
+        <p>You can download our latest newsletter <a href="<?= $this->sitepath('assets/downloads/HBCC-newsletter-10.pdf') ?>" download="HBCC Newsletter 10.pdf">here.</a></p>
+        
+        <h4>Annual Report 2018/19</h4>
+        <p>Download our latest annual report <a href="<?= $this->sitepath('assets/downloads/HBCC Annual Report December 2018.pdf') ?>" download="HBCC Annual Report 2018.pdf">here.</a></p>
+       
+    </section>    
+    
+    <section id="media">
+        <h4>Did you catch us on That's Lancashire?</h4>
+     
+       <iframe width="560" height="315" src="https://www.youtube.com/embed/rS_KvKR-afQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+ 
+    </section>    
+   <section id="copy">
+         <h4>Welcome</h4>
+        <p>Situated next to Hesketh Bank AFC's ground on Station Road, Hesketh Bank, the Community Centre consists of two large halls, providing safe, non-denominational facilities for the villages of Hesketh Bank, Banks, Tarleton and beyond.</p>
+        <p>Whether there is just a small number of you or up to a hundred people we can provide a room to meet your requirements.</p>
+        <p><strong>We currently provide facilities for:</strong></p>
+        <ul>
+            <li>Dance &amp; Fitness</li>
+            <li>Education &amp; Social</li>
+            <li>Meetings</li>
+            <li>After-school clubs</li>
+            <li>Local societies</li>
+        </ul>
+        <p>Additionally, our Multi-Use Games Area, or <a href="<?= $this->sitepath('our-facilities/muga') ?>">MUGA</a>, provides a safe environment for people of all ages to participate in football, basketball and other sports.</p>
+        <p><a href="<?= $this->sitepath('contact') ?>">Get in touch</a> to see how we can help.</p>
+        <section class="radio" style="text-align:center">
+           <h4>Did you catch us live on BBC Radio Lancashire? <span>Listen again below.</span></h4>
+         <audio controls>
+            <source src="<?= $this->sitepath('assets/audio/LANCS 2018 20 SEP HESKETH BANK.mp3') ?>" type="audio/mp3">
+        </audio>
+        </section>
+   </section>
+    <section id="calendar">
+        <h4>Upcoming Events</h4>
+        <p>We have a busy calendar of events with various courses, classes and clubs every week.</p>
+        <ul id="upcoming-events"></ul>
+    </section>    
+
+
+</section>    
